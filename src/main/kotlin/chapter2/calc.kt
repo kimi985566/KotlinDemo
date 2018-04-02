605562bb3677ba88b5dd14ea763ef833bfafbf88
Chengyu Yang <chengyu_yang@outlook.com>
2018-04-02 16:27:28 +0800

    Calculate

---
+++ src/main/kotlin/chapter2/calc.kt
@@ -0,0 +1,52 @@
+package chapter2
+
+fun main(args: Array<String>) {
+    while (true) {
+        try {
+            println("请输入算是例子：例如3 + 4")
+            val input = readLine() ?: break
+            val splits = input.split(" ")
+            if (splits.size < 3) {
+                throw IllegalArgumentException("参数个数不对")
+            }
+            val arg1 = splits[0].toDouble()
+            val opt = splits[1]
+            val arg2 = splits[2].toDouble()
+            println("$arg1 $opt $arg2 = ${Operate(opt).apply(arg1, arg2)}")
+        } catch (e: NumberFormatException) {
+            println("您确定输入的是数字吗？")
+        } catch (e: IllegalArgumentException) {
+            println("您确定输入的是三个参数吗？或者您确定您的输入是用空格分隔的吗？")
+        } catch (e: Exception) {
+            println("亲爱的用户，您的人品太差了，程序遇到了未知的异常，${e.message}")
+        }
+
+        println("再来一发?[Y]")
+        val cmd = readLine()
+        if (cmd == null || cmd.toLowerCase() != "y") {
+            break
+        }
+    }
+    println("感谢您使用我们的计算器。")
+}
+
+class Operate(op: String) {
+    val OpFun: (left: Double, Right: Double) -> Double
+
+    init {
+        OpFun = when (op) {
+            "+" -> { l, r -> l + r }
+            "-" -> { l, r -> l - r }
+            "*" -> { l, r -> l * r }
+            "/" -> { l, r -> l / r }
+            "%" -> { l, r -> l % r }
+            else -> {
+                throw UnsupportedClassVersionError(op)
+            }
+        }
+    }
+
+    fun apply(left: Double, Right: Double): Double {
+        return OpFun(left, Right)
+    }
+}
